abd38cd0a0bb2d3fe36b24fb9a1b71229cc38a0a
Achilleas Tsoumitas <knorcedger@gmail.com>
2016-01-13 21:38:57 +0200

    First commit

---
+++ app/index.js
@@ -0,0 +1,32 @@
+'use strict';
+var generators = require('yeoman-generator');
+
+module.exports = generators.Base.extend({
+	writing: {
+		angularGitignore: function() {
+			this.composeWith('knr-generator:angular-gitignore', {}, {
+				local: require.resolve('generator-angular-gitignore')
+			});
+		},
+		knrEslintrc: function() {
+			this.composeWith('knr-generator:knr-eslintrc', {}, {
+				local: require.resolve('generator-knr-eslintrc')
+			});
+		},
+		gitattributes: function() {
+			this.composeWith('knr-generator:gitattributes', {}, {
+				local: require.resolve('generator-gitattributes')
+			});
+		},
+		license: function() {
+			this.composeWith('knr-generator:license', {}, {
+				local: require.resolve('generator-license')
+			});
+		},
+		travis: function() {
+			this.composeWith('knr-generator:travis', {}, {
+				local: require.resolve('generator-travis')
+			});
+		}
+	}
+});
